48305213631f661916065e97bf00b5d9eaab21dc
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-02-18 00:45:40 +0600

    trainagle calculation and showing done

--- JS/calculation.js
+++ JS/calculation.js
@@ -1,4 +1,28 @@
-console.log("calculation file added");
+// triangle button get element by id
+document.getElementById("triangle-btn").addEventListener("click", function () {
+  // get triangleBase input field
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseString = triangleBaseInput.value;
+  const triangleBase = parseFloat(triangleBaseString);
+  triangleBaseInput.value = "";
 
-// triangle button get element by id 
+  // get triangle Hight input field
+  const triangleHeightInput = document.getElementById("traingle-height");
+  const triangleHeightString = triangleHeightInput.value;
+  const triangleHeight = parseFloat(triangleHeightString);
+  triangleHeightInput.value = "";
 
+  // make calculations for triangle
+  const triangleArea = 0.5 * triangleBase * triangleHeight;
+  // get result portal
+  const resultPortal = document.getElementById("result-portal");
+  const h6 = document.createElement("h6");
+  h6.innerHTML =
+    getNextSerialNumber() +
+    ". Traingle" +
+    " " +
+    triangleArea.toFixed(2) +
+    "<span>cm <sup>2</sup></span>";
+  // append in result portal
+  resultPortal.appendChild(h6);
+});
---
+++ JS/common.js
@@ -0,0 +1,7 @@
+// serial no
+let serialNumber = 0;
+// function to generate the next serial number
+function getNextSerialNumber() {
+  serialNumber++; // increment the serial number
+  return serialNumber;
+}
