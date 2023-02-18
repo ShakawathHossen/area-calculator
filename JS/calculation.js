4e63b318ffaf59bd497051a31fa8c8361960e6b3
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-02-18 14:28:57 +0600

    convert to metter square button adden dynamicly

--- JS/calculation.js
+++ JS/calculation.js
@@ -4,78 +4,75 @@ document.getElementById("triangle-btn").addEventListener("click", function () {
   const triangleBaseInput = document.getElementById("triangle-base");
   const triangleBaseString = triangleBaseInput.value;
   const triangleBase = parseFloat(triangleBaseString);
-    triangleBaseInput.value = "";
+  triangleBaseInput.value = "";
 
   // get triangle Hight input field
   const triangleHeightInput = document.getElementById("traingle-height");
   const triangleHeightString = triangleHeightInput.value;
   const triangleHeight = parseFloat(triangleHeightString);
-    triangleHeightInput.value = "";
+  triangleHeightInput.value = "";
+  if (triangleHeight < 0 ) {
+    alert("Please enter a valid positive number");
+    return;
+  }
 
   // make calculations for triangle
   const triangleArea = 0.5 * triangleBase * triangleHeight;
   // error handle for NAN and negative numbers
-  if(isNaN(triangleArea) || triangleArea<0 || triangleArea==''){
+  if (isNaN(triangleArea) || triangleArea < 0 || triangleArea == "") {
     alert("Please enter a valid positive number");
     return;
-
   }
   // get result portal
   const resultPortal = document.getElementById("result-portal");
   const p = document.createElement("p");
-  p.innerHTML =
-    getNextSerialNumber() +
-    ". Traingle" +
-    "  " +
-    triangleArea.toFixed(2) +
-    "<span>cm <sup>2</sup></span>";
+  p.innerHTML =getNextSerialNumber() +". Traingle" +"  " +triangleArea.toFixed(2) +"<span>cm <sup>2</sup></span>" +'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>";
   // append in result portal
   resultPortal.appendChild(p);
 });
 
-// rectangle card 
-// rectangle button get 
-document.getElementById("rectangle-btn").addEventListener("click", function() {
-    
+// rectangle card
+// rectangle button get
+document.getElementById("rectangle-btn").addEventListener("click", function () {
   // get rectangle width input field
   const rectangleWidthInput = document.getElementById("rectangle-width");
   const rectangleWidthString = rectangleWidthInput.value;
   const rectangleWidth = parseFloat(rectangleWidthString);
   rectangleWidthInput.value = "";
-  
+
   // get rectangleLength input field
   const rectangleLengthInput = document.getElementById("rectangle-length");
   const rectangleLengthString = rectangleLengthInput.value;
   const rectangleLength = parseFloat(rectangleLengthString);
   rectangleLengthInput.value = "";
 
-    // make calculations for rectangle
-    const rectangleArea =rectangleWidth * rectangleLength;
-    // error handle for NAN and negative numbers
-    if(isNaN(rectangleArea) || rectangleArea<0 || rectangleArea==''){
-        alert("Please enter a valid positive number");
-        return;
-    
-      }
-    // get result portal
-    const resultPortal = document.getElementById("result-portal");
-    const p = document.createElement("p");
-    p.innerHTML =
-      getNextSerialNumber() +
-      ". Rectangle" +
-      "  " +
-      rectangleArea.toFixed(2) +
-      "<span>cm <sup>2</sup></span>";
-    // append in result portal
-    resultPortal.appendChild(p);
-})
+  // make calculations for rectangle
+  const rectangleArea = rectangleWidth * rectangleLength;
+  // error handle for NAN and negative numbers
+  if (isNaN(rectangleArea) || rectangleArea < 0 || rectangleArea == "") {
+    alert("Please enter a valid positive number");
+    return;
+  }
+  // get result portal
+  const resultPortal = document.getElementById("result-portal");
+  const p = document.createElement("p");
+  p.innerHTML =
+    getNextSerialNumber() +
+    ". Rectangle" +
+    "  " +
+    rectangleArea.toFixed(2) +"<span>cm <sup>2</sup></span>"+'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>" ;
+  // append in result portal
+  resultPortal.appendChild(p);
+});
 
-// parallelogram card 
-// parallelogram-btn get 
-document.getElementById("parallelogram-btn").addEventListener("click", function() {
-    const parallelogramBase=10;
-    const parallegramHeight=12;
-    const parallegramArea=parallelogramBase*parallegramHeight;
+// parallelogram card
+// parallelogram-btn get
+document
+  .getElementById("parallelogram-btn")
+  .addEventListener("click", function () {
+    const parallelogramBase = 10;
+    const parallegramHeight = 12;
+    const parallegramArea = parallelogramBase * parallegramHeight;
 
     // get result portal
     const resultPortal = document.getElementById("result-portal");
@@ -85,69 +82,63 @@ document.getElementById("parallelogram-btn").addEventListener("click", function(
       ". Parallelogram" +
       "  " +
       parallegramArea.toFixed(2) +
-      "<span>cm <sup>2</sup></span>";
+      "<span>cm <sup>2</sup></span>"+'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>";
     // append in result portal
     resultPortal.appendChild(p);
+  });
 
-})
+// Rhombus card
+// rhombus-btn get
+document.getElementById("rhombus-btn").addEventListener("click", function () {
+  const diagonalOne = 10;
+  const diagonalTwo = 12;
+  const rhombusArea = 0.5 * diagonalOne + diagonalTwo;
 
-// Rhombus card 
-// rhombus-btn get 
-document.getElementById("rhombus-btn").addEventListener("click", function() {
-    const diagonalOne=10;
-    const diagonalTwo=12;
-    const rhombusArea=0.5*diagonalOne+diagonalTwo;
+  // get result portal
+  const resultPortal = document.getElementById("result-portal");
+  const p = document.createElement("p");
+  p.innerHTML =
+    getNextSerialNumber() +
+    ". Rhombus" +
+    "  " +
+    rhombusArea.toFixed(2) +
+    "<span>cm <sup>2</sup></span>"+'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>";
+  // append in result portal
+  resultPortal.appendChild(p);
+});
+// Pentagon card
+// pentagon-btn get
+document.getElementById("pentagon-btn").addEventListener("click", function () {
+  const pentagonPath = 6;
+  const pentagonBase = 10;
+  const pentagonArea = 0.5 * pentagonPath * pentagonBase;
 
-    // get result portal
-    const resultPortal = document.getElementById("result-portal");
-    const p = document.createElement("p");
-    p.innerHTML =
-      getNextSerialNumber() +
-      ". Rhombus" +
-      "  " +
-      rhombusArea.toFixed(2) +
-      "<span>cm <sup>2</sup></span>";
-    // append in result portal
-    resultPortal.appendChild(p);
+  // get result portal
+  const resultPortal = document.getElementById("result-portal");
+  const p = document.createElement("p");
+  p.innerHTML =
+    getNextSerialNumber() +
+    ". Pentagon" +
+    "  " +
+    pentagonArea.toFixed(2) +"<span>cm <sup>2</sup></span>"+'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>";
+  // append in result portal
+  resultPortal.appendChild(p);
+});
+// Ellipse card
+// ellipse-btn get
+document.getElementById("ellipse-btn").addEventListener("click", function () {
+  const ellipseA = 10;
+  const ellipseB = 4;
+  const ellipseArea = 3.1416 * ellipseA * ellipseB;
 
-})
-// Pentagon card 
-// pentagon-btn get 
-document.getElementById("pentagon-btn").addEventListener("click", function() {
-    const pentagonPath=6;
-    const pentagonBase=10;
-    const pentagonArea=0.5*pentagonPath*pentagonBase;
-
-    // get result portal
-    const resultPortal = document.getElementById("result-portal");
-    const p = document.createElement("p");
-    p.innerHTML =
-      getNextSerialNumber() +
-      ". Pentagon" +
-      "  " +
-      pentagonArea.toFixed(2) +
-      "<span>cm <sup>2</sup></span>";
-    // append in result portal
-    resultPortal.appendChild(p);
-
-})
-// Ellipse card 
-// ellipse-btn get 
-document.getElementById("ellipse-btn").addEventListener("click", function() {
-    const ellipseA=10;
-    const ellipseB=4;
-    const ellipseArea=3.1416*ellipseA*ellipseB
-
-    // get result portal
-    const resultPortal = document.getElementById("result-portal");
-    const p = document.createElement("p");
-    p.innerHTML =
-      getNextSerialNumber() +
-      ". Ellipse" +
-      "  " +
-      ellipseArea.toFixed(2) +
-      "<span>cm <sup>2</sup></span>";
-    // append in result portal
-    resultPortal.appendChild(p);
-
-})
+  // get result portal
+  const resultPortal = document.getElementById("result-portal");
+  const p = document.createElement("p");
+  p.innerHTML =
+    getNextSerialNumber() +
+    ". Ellipse" +
+    "  " +
+    ellipseArea.toFixed(2) +"<span>cm <sup>2</sup></span>"+'   '+' '+ "<span style='color: white; background-color: #0B5ED7; padding: 6px 8px; border-radius: 2px;' >Convet to m<sup>2</sup></span>";
+  // append in result portal
+  resultPortal.appendChild(p);
+});
--- JS/common.js
+++ JS/common.js
@@ -5,6 +5,20 @@ function getNextSerialNumber() {
   serialNumber++; // increment the serial number
   return serialNumber;
 }
+// button on result portal 
+function createButton() {
+  // Create a button element
+  const button = document.createElement("button");
+  button.InnerHTML = "convert";
+  // Add some styles to the button
+  button.style.backgroundColor = "blue";
+  button.style.color = "white";
+  button.style.padding = "10px";
+  button.style.border = "none";
+  button.style.borderRadius = "5px";
+  return button;
+}
+
 // blog button interactions
 function getBlogButton() {
   window.location.href = "blog.html";
@@ -16,7 +30,7 @@ function getBackHomeButton() {
 
 // color chgane on mouseover
 document.getElementById("random1").addEventListener('mouseenter',function() {
-  colors='rgb('+Math.round(Math.random() * 255)+','+Math.round(Math.random() * 255)+','+Math.round(Math.random() * 255)+')';
+  const colors='rgb('+Math.round(Math.random() * 255)+','+Math.round(Math.random() * 255)+','+Math.round(Math.random() * 255)+')';
   this.style.background =colors;
 })
 // color chgane on mouseover
